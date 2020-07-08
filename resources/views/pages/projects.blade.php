fd4052568b6a2f4a3395f27a72953efa5a4cc1d5
emmaukachi <emmaukachi2015@gmail.com>
2020-07-08 16:17:24 +0100

    added new images to the image folder and edited the projects page

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
     <div class="page-content text-center">
     <h2>Specialized projects</h2>
-    <p>Construction & Building Html Template.</p>
+    <p>Construction & Building.</p>
     </div>
     </div>
     </section>
@@ -14,45 +14,45 @@
     <div class="row">
     <div class="col-lg-4 col-sm-6 padding-15">
     <div class="project-item">
-    <img src="/img/project-1.jpg" alt="projects">
+    <img src="/img/3D3.jpg" alt="projects">
     <div class="overlay"></div>
     <a href="#" class="view-icon ajax-popup-link"> <i class="fas fa-expand"></i></a>
     <div class="projects-content">
     <a href="#" class="category">Interior</a>
-    <h3><a href="#" class="tittle">Rectangular house near italy</a></h3>
+    {{--<h3><a href="#" class="tittle">Rectangular house near italy</a></h3>--}}
     </div>
     </div>
     </div>
     <div class="col-lg-4 col-sm-6 padding-15">
     <div class="project-item">
-    <img src="/img/project-2.jpg" alt="projects">
+    <img src="/img/3DView5.jpg" alt="projects">
     <div class="overlay"></div>
     <a href="#" class="view-icon"> <i class="fas fa-expand"></i></a>
     <div class="projects-content">
     <a href="#" class="category">Architecture</a>
-    <h3><a href="#" class="tittle">Cathedral of brasilia brasilia</a></h3>
+    {{--<h3><a href="#" class="tittle">Cathedral of brasilia brasilia</a></h3>--}}
     </div>
     </div>
     </div>
     <div class="col-lg-4 col-sm-6 padding-15">
     <div class="project-item">
-    <img src="/img/project-3.jpg" alt="projects">
+    <img src="/img/g-8.jpg" alt="projects">
     <div class="overlay"></div>
     <a href="#" class="view-icon"> <i class="fas fa-expand"></i></a>
     <div class="projects-content">
     <a href="#" class="category">Design</a>
-    <h3><a href="#" class="tittle">Harpa concert hall reykjavik</a></h3>
+    {{--<h3><a href="#" class="tittle">Harpa concert hall reykjavik</a></h3>--}}
     </div>
     </div>
     </div>
     <div class="col-lg-4 col-sm-6 padding-15">
     <div class="project-item">
-    <img src="/img/project-4.jpg" alt="projects">
+    <img src="/img/g-2.jpg" alt="projects">
     <div class="overlay"></div>
     <a href="#" class="view-icon"> <i class="fas fa-expand"></i></a>
     <div class="projects-content">
     <a href="#" class="category">Architecture</a>
-    <h3><a href="#" class="tittle">Milwauke museum wisconsin</a></h3>
+    {{--<h3><a href="#" class="tittle">Milwauke museum wisconsin</a></h3>--}}
     </div>
     </div>
     </div>
@@ -63,7 +63,7 @@
     <a href="#" class="view-icon"> <i class="fas fa-expand"></i></a>
     <div class="projects-content">
     <a href="#" class="category">Design</a>
-    <h3><a href="#" class="tittle">The dancing house prague</a></h3>
+    {{--<h3><a href="#" class="tittle">The dancing house prague</a></h3>--}}
     </div>
     </div>
     </div>
@@ -74,7 +74,7 @@
     <a href="#" class="view-icon"> <i class="fas fa-expand"></i></a>
     <div class="projects-content">
     <a href="#" class="category">Design</a>
-    <h3><a href="#" class="tittle">The dancing house prague</a></h3>
+    {{--<h3><a href="#" class="tittle">The dancing house prague</a></h3>--}}
     </div>
     </div>
     </div>
